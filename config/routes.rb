c43318989172aa435ac2f2a1e9a913c4f76210c8
mireie <michael.reiersgaard@gmail.com>
2021-08-30 19:12:05 -0700

    add devise, home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/show'
   resources :events
   resources :jobs
+  
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_014253) do
+ActiveRecord::Schema.define(version: 2021_08_31_015942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_014253) do
     t.date "response_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
