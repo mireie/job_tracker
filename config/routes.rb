a3e27cc123f11e9b4dbe318239917c17df6b4793
mireie <michael.reiersgaard@gmail.com>
2021-09-02 09:25:52 -0700

    root for the home route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :events
   resources :jobs
   
-  root to: "home#index"
+  root to: "jobs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
