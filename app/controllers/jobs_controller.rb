6d9ef3803fee8a7e8dd897b3208e703c75d6bcb7
mireie <michael.reiersgaard@gmail.com>
2021-09-22 12:17:01 -0700

    add active/inactive add select form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: %i[ show edit update destroy ]
+  
 
   # GET /jobs or /jobs.json
   def index
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  scope :active, -> { where(status: 'Applied') }
+  scope :inactive, -> { where.not(status: 'Applied') }
 end
