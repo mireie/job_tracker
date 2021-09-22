11a543e2a55b04ddf6db4094633f976059755ad7
mireie <michael.reiersgaard@gmail.com>
2021-09-22 12:50:46 -0700

    update admin privileges

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,7 +7,7 @@ class JobsController < ApplicationController
 
   # GET /jobs or /jobs.json
   def index
-    @jobs = Job.all
+    @jobs = Job.order(created_at: :desc)
   end
 
   # GET /jobs/1 or /jobs/1.json
