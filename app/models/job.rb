e7de05a2a10e0dacd8634ad0df6c35a3d97e7df6
mireie <michael.reiersgaard@gmail.com>
2021-09-24 08:24:12 -0700

    add this week

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
   scope :active, -> { where(status: 'Applied') }
   scope :inactive, -> { where.not(status: 'Applied') }
+  scope :recent, -> { where("created_at >=?", 1.week.ago)}
 end
