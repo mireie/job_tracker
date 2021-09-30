fa0d81a627019daf05803f515d1325b078b2280d
mireie <michael.reiersgaard@gmail.com>
2021-09-30 10:32:41 -0700

    add next/previous week

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
   scope :active, -> { where(status: 'Applied') }
   scope :inactive, -> { where.not(status: 'Applied') }
-  scope :recent, -> { where("created_at >=?", Date.today.beginning_of_week)}
+  scope :this_week, -> { where("created_at >=?", Date.today.beginning_of_week)}
+  scope :last_week, -> { where("created_at BETWEEN ? AND ?", Date.today.prev_occurring(:monday).prev_occurring(:monday), Date.today.beginning_of_week)}
 end
