3b8cc648fb3fde2f8572820fcd649ab2ce1793c6
mireie <michael.reiersgaard@gmail.com>
2021-09-30 10:16:49 -0700

    update apps this week

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
   scope :active, -> { where(status: 'Applied') }
   scope :inactive, -> { where.not(status: 'Applied') }
-  scope :recent, -> { where("created_at >=?", 1.week.ago)}
+  scope :recent, -> { where("created_at >=?", Date.today.beginning_of_week)}
 end
