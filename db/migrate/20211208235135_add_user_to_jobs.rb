2703bc39bd405bc2bc0a1d8a4454c57aa5dc850d
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-12-08 23:59:18 -0800

    fix squash

--- db/migrate/20211208235135_add_user_to_jobs.rb
+++ db/migrate/20211208235135_add_user_to_jobs.rb
@@ -2,6 +2,7 @@ class AddUserToJobs < ActiveRecord::Migration[6.1]
   def change
     add_reference :jobs, :user, foreign_key: true
     add_column :users, :token, :string
-    add_column :users, :token, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
