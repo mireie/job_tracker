a3231b50382af6bffeb750789e628eb88e426fcb
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-12-09 14:14:41 -0800

    update seed + factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-User.destroy_all
-FactoryBot.create(:admin)
+User.where("RIGHT(last_name::varchar, 5) = '_test'"
+).destroy_all
+FactoryBot.create(:admin) if User.find_by(email: 'admin@example.com').nil?
 FactoryBot.create_list(:user_with_jobs, 25)
 FactoryBot.create_list(:job, 25, user: User.first)
 puts "Seed Successful."
