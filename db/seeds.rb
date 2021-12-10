1295617e318caee86f638372a3319c10398e88cd
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-12-10 13:52:31 -0800

    clean up seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-User.where("RIGHT(last_name::varchar, 5) = '_test'"
-).destroy_all
-FactoryBot.create(:admin) if User.find_by(email: 'admin@example.com').nil?
+User.where("RIGHT(last_name::varchar, 5) = '_test'").destroy_all
+if !Rails.env.production?
+  FactoryBot.create(:admin) if User.find_by(email: 'admin@example.com').nil?
+end
 FactoryBot.create_list(:user_with_jobs, 25)
-FactoryBot.create_list(:job, 25, user: User.first)
 puts "Seed Successful."
