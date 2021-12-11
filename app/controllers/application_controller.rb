8ba65bf6b22c4cc32acb38a702e293210d86e45c
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-12-11 14:08:57 -0800

    update sign-in style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def authorize_admin
     if signed_in? == false
       flash[:alert] = "Please sign in to continue"
@@ -18,5 +20,10 @@ class ApplicationController < ActionController::Base
         end
       end
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
