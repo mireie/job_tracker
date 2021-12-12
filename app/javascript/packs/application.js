3fa533f833260f5ec94099ee63d8fefff6fefb83
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-12-12 13:14:52 -0800

    update bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,33 +12,13 @@
 
 
 
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import 'bootstrap-icons/font/bootstrap-icons.css'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('ServiceWorker registered: ', registration);
-
-    var serviceWorker;
-    if (registration.installing) {
-      serviceWorker = registration.installing;
-      console.log('Service worker installing.');
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-      console.log('Service worker installed & waiting.');
-    } else if (registration.active) {
-      serviceWorker = registration.active;
-      console.log('Service worker active.');
-    }
-  }).catch(registrationError => {
-    console.log('Service worker registration failed: ', registrationError);
-  });
-});
