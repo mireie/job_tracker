a45b1c4613b9cc7a28712c2f0e70b9fc46d421c8
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-12-12 18:50:21 -0800

    add public link

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :events
   resources :jobs
 
-  get '/share/:user/create', action: :create, controller: 'public_share'
-  get '/share/:user/remove', action: :remove, controller: 'public_share'
-  get '/share/:user/:token', action: :show, controller: 'public_share'
+  get '/share/:user/create', action: :create, controller: 'public_share', as: 'share_create'
+  get '/share/:user/remove', action: :remove, controller: 'public_share', as: 'share_remove'
+  get '/share/:user/:token', action: :show, controller: 'public_share', as: 'share_show'
   
   root to: "jobs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
