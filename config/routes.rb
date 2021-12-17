6f59a128168f3fbbf82d47b926d316791bf759ef
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-12-17 13:10:06 -0800

    add quick add url route

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/show'
   resources :events
-  get 'jobs/quick-add', action: :quick_add, controller: 'jobs', as: 'quick_add'
+  get 'jobs/quick-add', action: :quick_add, controller: 'jobs'
   resources :jobs
 
   get '/share/:user/create', action: :create, controller: 'public_share', as: 'share_create'
   get '/share/:user/remove', action: :remove, controller: 'public_share', as: 'share_remove'
   get '/share/:user/:token', action: :show, controller: 'public_share', as: 'share_show'
+  get 'quick-add', action: :quick_add, controller: 'jobs', as: 'quick_add'
   
   root to: "jobs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
