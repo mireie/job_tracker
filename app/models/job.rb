f1bc6a03a42634b6537a7a2f28dc8fcbb7453e9a
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-12-21 19:29:42 -0800

    add statuses to scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,8 @@
 class Job < ApplicationRecord
-  scope :active, -> { where(status: 'Applied') }
-  scope :inactive, -> { where.not(status: 'Applied') }
+  @@active_statuses = ['Applied','Interviewing','Have Not Applied','Other']
+  
+  scope :active, -> { where(status: @@active_statuses) }
+  scope :inactive, -> { where.not(status: @@active_statuses) }
   scope :this_week, -> { where("created_at >=?", Date.today.beginning_of_week)}
   scope :last_week, -> { where("created_at BETWEEN ? AND ?", Date.today.prev_occurring(:monday).prev_occurring(:monday), Date.today.beginning_of_week)}
 
